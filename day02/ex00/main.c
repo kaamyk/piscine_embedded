d4358dfcc2208322b1343f4983bf4ef17f703f33
Antoine VINCENT <anvincen@made-f0Dr11s6.clusters.42paris.fr>
2025-03-05 18:46:38 +0100

    Rename day folder + begin day02

---
+++ day02/ex00/main.c
@@ -0,0 +1,57 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+void	uart_init( void )
+{
+	// Check there is on other communication going on
+	//	Turn on the Transmitter and Receiver bits	
+	UCSR0B |= (1 << TXEN0) | (1 << RXEN0);
+	//	Set the character size to 8 bits
+	UCSR0C |= (3 << UCSZ00);
+	//	(not mandatory) Set a UNIQUE stop bit
+	UCSR0C &= ~(1 << USBS0);
+	//	(not mandatory) Disable the parity
+	UCSR0B &= ~(3 << UPM00);
+}
+
+void	uart_tx( void )
+{
+	//	while data register si not empty
+	PORTB |= (1 << DDB0);
+	while (!(UCSR0A & (1 << UDRE0)));
+	PORTB &= ~(1 << DDB0);
+	UDR0 = 'Z';
+}
+
+void	define_timer_1Hz( void )
+{
+	// Timer1 => mode 4 (CTC), / 256, compare match 
+	TCCR1A |= (1 << COM1A0) | (1 << COM1A1);
+	TCCR1B |= (1 << WGM12) | (1 << CS12) | (1 << CS10);
+	TCNT1 = 0;
+	OCR1A = 15624;
+	TIMSK1 |= (1 << OCIE1A);
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	uart_tx();
+}
+
+int		main ( void )
+{
+
+	// debug
+	DDRB |= (1 << DDB1);
+	
+	//	Calculate the Brr
+	const uint16_t ubrr = (F_CPU / (16 * UART_BAUDRATE));
+	UBRR0H = (uint8_t)(ubrr >> 8);
+	UBRR0L = (uint8_t)ubrr;
+	define_timer_1Hz();
+	//	Enable interuptions
+	sei();
+	uart_init();
+	
+	while(1);
+}
