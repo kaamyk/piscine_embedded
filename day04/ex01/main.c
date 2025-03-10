92a1f154f60a1b68cb297aeb1748c64e69b38fb2
anvincen <antvin100.av@gmail.com>
2025-03-10 07:58:13 +0100

    Working on day04/ex01

--- day04/ex01/main.c
+++ day04/ex01/main.c
@@ -1,32 +1,37 @@
 #include "include.h"
 
 uint8_t		up = 0;	// Toggle avec un timer 1Hz
+uint8_t		t0_ovf_counter = 0;
 
 void	init_timers( void )
 {
-	//	Prescaler
-		//	64
-	CLKPR = (1 << CLKPCE);
-	CLKPR = (1 << CLKPS2) | (1 << CLKPS1);
-	
 	//	Timer0
-		//	divider 1024
-	TCCR0A |= (1 << COM0A1) | (1 << COM0A0)
+		//	PWM, phase correct, TOP=255
+	TCCR0A |= (1 << WGM00);
 		
 	//	Timer1
-		//	clear bot, set comp | mode 14 (Fast pwm, top ICR1) | divider 8
+		//	set bot, clear comp | mode 14 (Fast pwm, top ICR1) 
 	TCCR1A |= (1 << COM1A1) | (1 << WGM11);
-	TCCR1B |= (1 << WGM12) | (1 << WGM13) | (1 << CS11);
+	TCCR1B |= (1 << WGM12) | (1 << WGM13) | (1 << CS12);
+	ICR1 = 255;
 }
 
-ISR()	// Iterrupt toggle up/down
+ISR(TIMER0_OVF_vect)
 {
-	up ^= 1;
+	t0_ovf_counter++;
+	PORTB |= (1 << PB0);
+	if (t0_ovf_counter >= 122)
+	{
+		up ^= 1;
+		t0_ovf_counter ^= t0_ovf_counter;
+		up & 1 ? OCR1A++ : OCR1A--;
+	}
 }
 
 int		main ( void )
 {
-	
-	
+	DDRB |= (1 << DDB0);
+	init_timers();
+	sei();
 	while (1);
 }
