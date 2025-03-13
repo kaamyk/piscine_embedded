5c37d4707ff007ba70d46fbdfea8d5689a9d3960
anvincen <antvin100.av@gmail.com>
2025-03-14 00:24:14 +0100

    day07/ex01 done

--- day07/ex01/main.c
+++ day07/ex01/main.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
-char	input[256] = {0};
-uint8_t	input_counter = 0;
-uint8_t	input_done = 0;
+uint8_t input_done = 0;
 
-uint8_t	eeprom_read( const uint16_t addr )
+uint8_t eeprom_read(const uint16_t addr)
 {
 	while (EECR & (1 << EEPE));
 	EEAR = addr & 0x1ff;
@@ -12,7 +10,7 @@ uint8_t	eeprom_read( const uint16_t addr )
 	return (EEDR);
 }
 
-void	eeprom_write( const uint16_t addr, const uint8_t byte )
+void eeprom_write(const uint16_t addr, const uint8_t byte)
 {
 	while (EECR & (1 << EEPE));
 	while (SPMCSR & (1 << SELFPRGEN));
@@ -22,25 +20,25 @@ void	eeprom_write( const uint16_t addr, const uint8_t byte )
 	EECR |= (1 << EEPE);
 }
 
-void	print_addr( const uint16_t addr )
+void print_addr(const uint16_t addr)
 {
 	uart_printstr("0000");
 	print_hex_value_16(addr);
 	uart_tx(' ');
 }
 
-void	eeprom_read_range( const uint16_t start, const uint16_t range )
+void eeprom_read_range(const uint16_t start, const uint16_t range)
 {
-	uint8_t	data = 0;
-	
-	for (uint16_t i = start; i < 0x03ff && i < range; i++)
+	uint8_t data = 0;
+
+	for (uint16_t i = start; i <= 0x03ff && i <= range; i++)
 	{
 		if ((start + i) % 16 == 0)
 		{
 			uart_nl();
 			print_addr(start + i);
 		}
-		else if (((start + i) % 2) == 0) 
+		else if (((start + i) % 2) == 0)
 			uart_tx(' ');
 		data = eeprom_read(start + i);
 		print_hex_value_8(data);
@@ -48,53 +46,86 @@ void	eeprom_read_range( const uint16_t start, const uint16_t range )
 	uart_nl();
 }
 
-uint16_t		atos_base( const char *s )
+uint8_t	is_hexa(const char c)
 {
-	uint16_t		first_digit = strchr(BASE, s[0]) - BASE;
-	uint16_t		second_digit = strchr(BASE, s[1]) - BASE;
-	uint16_t		third_digit = strchr(BASE, s[2]) - BASE;
-	uint16_t		quad_digit = strchr(BASE, s[4]) - BASE;
-	
-	return (((first_digit & 0x0F) << 12) | ((second_digit & 0x0F) << 8) | \
-			((third_digit & 0x0F) << 8) | (quad_digit & 0x0F));
+	return ((c >='a' && c <='f') || (c >='0' && c <= '9'));
 }
 
-uint8_t		handle_input( uint16_t *w_addr, uint8_t *w_byte )
+uint16_t atos_base(const uint8_t *s)
 {
-	uint8_t i = 0;
-	char	s_tmp[4] = {0};
-	for (; i < 4 && input[i] != 0 && input[i] != ' '; i++)
+	uint16_t digits[4] = {0};
+	uint8_t	 i		   = 0;
+
+	while (i < 4)
 	{
-		if ((input[i] < '0' || input[i] > '9') && (input [i] < 'a' || input[i] > 'f'))
-		{
-			uart_printstr("Address: wrong digit\n\r");
-			return (1);
-		}
+		digits[i] = (strchr(BASE, *s) - BASE);
+		s++;
+		i++;
+	}
+
+	return (((digits[0] & 0x0F) << 12) | ((digits[1] & 0x0F) << 8) |
+			((digits[2] & 0x0F) << 4) | (digits[3] & 0x0F));
+}
+
+uint8_t atob_base(const char *s)
+{
+	uint16_t first_digit  = strchr(BASE, s[0]) - BASE;
+	uint16_t second_digit = strchr(BASE, s[1]) - BASE;
+
+	return (((first_digit & 0x0F) << 4) | (second_digit & 0x0F));
+}
+
+uint16_t get_addr(const char input[256])
+{
+	uint8_t s_tmp[4] = {0};
+	uint8_t i		 = 0;
+
+	while (i < 4 && input[i] != '\0')
+	{
 		s_tmp[i] = input[i];
+		i++;
 	}
 	if (input[i] != ' ')
 	{
-		uart_printstr("Address: too big\r\n");
+		uart_printstr("Address: Wrong format\r\n");
+		return (0xffff);
+	}
+	
+	return (atos_base(s_tmp));
+}
+
+uint8_t handle_input(const char input[256], uint16_t *w_addr, uint8_t *w_byte)
+{
+	uint8_t i = 0;
+
+	*w_addr = get_addr(input);
+	uart_nl();
+	if (*w_addr == 0xffff)
+		return (1);
+	if (*w_addr > 0x03ff)
+	{
+		uart_printstr("Address: too high\r\n");
 		return (1);
 	}
-	*w_addr = atos_base(s_tmp);
-	i++;
-	if (input[i] == 0 || input[i + 1] == 0)
+	while (input[i++] != ' ');
+	if (input[i] == 0 || is_hexa(input[i]) == 0 
+		|| input[i + 1] == 0 || is_hexa(input[i + 1]) == 0)
 	{
 		uart_printstr("Byte: wrong format or empty\r\n");
 		return (1);
 	}
-	*w_byte = atos_base(input + i) & 0x00ff;
+	*w_byte = atob_base(input + i);
 	return (0);
 }
 
-void get_input( void )
+void get_input(char input[256])
 {
-	char c = 0;
+	uint8_t input_counter = 0;
+	char	c			  = 0;
+
 	while (input_done == 0)
 	{
-		while (c == 0)
-			c = uart_rx();
+		while (c == 0) c = uart_rx();
 		if (c == '\n' || c == '\r')
 		{
 			uart_tx('\r');
@@ -109,9 +140,9 @@ void get_input( void )
 				input_counter--;
 			}
 		}
-		else if (c == ' ' || (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f'))
+		else if (c == ' ' || is_hexa(c) == 1)
 		{
-			if (input_counter <= 7)
+			if (input_counter < 7)
 			{
 				uart_tx(c);
 				input[input_counter++] = c;
@@ -121,26 +152,26 @@ void get_input( void )
 	}
 }
 
-int		main( void )
+int main(void)
 {
-	//debug
-	DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB4);
-	
-	uint16_t	w_addr = 0;
-	uint8_t		w_byte = 0;
-	
+	char	 input[256] = {0};
+	uint16_t w_addr		= 0;
+	uint8_t	 w_byte		= 0;
+
 	init_uart();
 	while (1)
 	{
-		get_input();
+		get_input(input);
 		if (input_done == 1)
 		{
-			handle_input(&w_addr, &w_byte);
-			print_hex_value_16(w_addr);
-			print_hex_value_8(w_byte);
-			eeprom_read_range(0x00, 0x03ff);
-			input_counter = 0;
 			input_done = 0;
+			w_addr = 0;
+			w_byte = 0;
+			if (handle_input(input, &w_addr, &w_byte) == 0)
+			{
+				eeprom_write(w_addr, w_byte);
+				eeprom_read_range(0x00, 0x03ff);
+			}
 		}
 	}
 }
